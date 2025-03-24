f79542e655aa102ca88340d4127c802d920ae2f0
dfromm91 <142464779+dfromm91@users.noreply.github.com>
2025-03-23 21:08:11 -0500

    Update settings.py to exclude vercel and fly.io

--- DnDnDB/settings.py
+++ DnDnDB/settings.py
@@ -45,14 +45,12 @@ INSTALLED_APPS = [
 APP_NAME = os.getenv("FLY_APP_NAME", None)
 
 CSRF_TRUSTED_ORIGINS = [
-    f"https://{APP_NAME}.fly.dev",  # ✅ App nae
-    'https://art-social-seven.vercel.app',  # ✅ Add frontend domain
-    "https://v54-tier3-team-38.onrender.com/"
+    "https://v54-tier3-team-38.onrender.com"
 ] if APP_NAME else []
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
-    'https://v54-tier3-team-38.onrender.com/',
+    'https://v54-tier3-team-38.onrender.com',
 ]
 CORS_ALLOW_METHODS = [
 'GET',
