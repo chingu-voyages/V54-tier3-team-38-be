bd268f4d3df3a56fdcc8f01fc2b8bd2a7639b87e
Eoin McDonnell <oldmcdonnell@gmail.com>
2025-03-31 19:46:47 -0400

    fixed another amind issue, importing for preview.

--- app_DnDnDB_app/admin.py
+++ app_DnDnDB_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Asset, SessionData, PageData
+from django.utils.html import format_html
 
 @admin.register(Asset)
 class AssetAdmin(admin.ModelAdmin):
@@ -12,7 +13,7 @@ class AssetAdmin(admin.ModelAdmin):
             return format_html('<img src="{}" width="50" height="50" />', obj.image.url)
         return "-"
     thumbnail.short_description = 'Preview'
-    
+
 @admin.register(SessionData)
 class SessionDataAdmin(admin.ModelAdmin):
     list_display = ('session_id', 'created_at')
